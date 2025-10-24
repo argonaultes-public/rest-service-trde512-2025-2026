9b67ebe9061c00f264e91e43c3f1920389414b64
argonaultes <gael@argonaultes.fr>
2025-10-24 17:40:39 +0200

    configure database

--- src/test/java/fr/argonaultes/restservice/RestIT.java
+++ src/test/java/fr/argonaultes/restservice/RestIT.java
@@ -1,14 +1,24 @@
 package fr.argonaultes.restservice;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@JdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class RestIT {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     void testIfDbExists() {
         if (System.getenv("DATABASE_URL_TEST").isBlank()) {
+            jdbcTemplate.execute("SELECT 2");
             fail("Missing env variable DATABASE_URL_TEST");
         }
 
